c3daa7d511917829ae634742fc8922905cc8a945
calla-a <rrosalesguzmn@byupathway.edu>
2025-08-07 17:41:46 -0600

    Shapes Program

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,24 @@
 using System;
+using Shapes;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        Square square = new Square("red", 5);
+        Rectangle rectangle = new Rectangle("blue", 8, 5);
+        Circle circle = new Circle("yellow", 5);
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+            Console.WriteLine();
+        }
     }
 }
---
+++ week06/Shapes/Shape.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    public class Shape
+    {
+        private string _color;
+
+        public Shape(string color)
+        {
+            _color = color;
+        }
+        public string GetColor()
+        {
+            return _color;
+        }
+
+        public void SetColor(string color)
+        {
+            _color = color;
+        }
+
+        public virtual double GetArea()
+        {
+            return 0;
+        }
+    }
+}
